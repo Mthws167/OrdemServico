fd4b721ffcb5d39787888a3b0e813a42a6f461e0
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-27 01:15:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,35 +22,15 @@ function numeroCasa(evt) {
     }
 }
 
-
-function Formulario() {
-    var NOS = document.getElementById('NOS').innerHTML = NOS;
-    var Nome = document.getElementById('nome');
-    var Endereco = document.getElementById('endereco');
-    var NumeroCasa = document.getElementById('numero');
-    var Bairro = document.getElementById('bairro');
-    var Cidade = document.getElementById('cidade');
-    var Estado = document.getElementById('estado');
-    var CEP = document.getElementById('cep');
-    var CPFCNPJ = document.getElementById('CpfCnpj');
-    var Cliente = document.getElementById('cliente');
-    var Descricao = document.getElementById('descricao');
-    var Servico = document.getElementById('servico');
-    var Data = document.getElementById('data');
-    var Hora = document.getElementById('hora');
-
-}
+var CpfCnpjMask = function (val) {
+    return val.replace(/\D/g, '').length <= 11 ? '000.000.000-009' : '00.000.000/0000-00';
+    },
+    cpfCnpjpOpcao = {
+    onKeyPress: function(val, e, field, options) {
+    field.mask(CpfCnpjMask.apply({}, arguments), options);
+    }
+    };
 
 
-function gohome() {
-    window.location = 'index.html';
-}
-function save() {
-    window.localStorage.setItem('campo1', $('#campo1').val());
-}
-function load() {
-    $('#campo2').val(window.localStorage.getItem('campo1'));
-}
-function erase() {
-    window.localStorage.removeItem('campo1');
-}
+
+
