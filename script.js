6bcaccd985115355dcd2a42916c8f4484cf47814
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-27 20:59:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,42 @@ function numeroCasa(evt) {
     }
 }
 
+(function () {
+    function adicionar() {
+        var dados = JSON.stringify({
+            nos: document.querySelector("input[name=nos]").value,
+            nome: document.querySelector("input[name=nome]").value,
+            endereco: document.querySelector("input[name=endereco]").value,
+            numero: document.querySelector("input[name=numero]").value,
+            bairro: document.querySelector("input[name=bairro]").value,
+            cidade: document.querySelector("input[name=cidade]").value,
+            estado: document.querySelector("input[name=estado]").value,
+            cep: document.querySelector("input[name=cep]").value,
+            telefone: document.querySelector("input[name=telefone]").value,
+            cpfcnpj: document.querySelector("input[name=cpfcnpj]").value,
+            cliente: document.querySelector("input[name=cliente]").value,
+            telefone2: document.querySelector("input[name=telefone2]").value,
+            cpfcnpj2: document.querySelector("input[name=cpfcnpj2]").value,
+            obs: document.querySelector("input[name=obs]").value,
+            descricao: document.querySelector("input[name=descricao]").value,
+            servico: document.querySelector("input[name=servico]").value,
+            data: document.querySelector("input[name=data]").value,
+            hora: document.querySelector("input[name=hora]").value
+
+        });
+        // tbClientes.push(dados);
+        localStorage.setItem("storage", JSON.stringify(dados));
+        alert("Registro Adicionado!");
+        return true;
+    }
+    var form = document.querySelector("form");
+    form.addEventListener("submit", function () {
+        // event.preventDefault(); event
+        return adicionar();
+    });
+})();
+
+
 var CpfCnpjMask = function (val) {
     return val.replace(/\D/g, '').length <= 11 ? '000.000.000-009' : '00.000.000/0000-00';
 },
@@ -31,63 +67,3 @@ var CpfCnpjMask = function (val) {
         }
     };
 
-
-function resultado() {
-    var nos = document.getElementById("NOS").value;
-    var nome = document.getElementById("nome").value;
-    var endereco = document.getElementById("endereco").value;
-    var numero = document.getElementById("numero").value;
-    var bairro = document.getElementById("bairro").value;
-    var cidade = document.getElementById("cidade").value;
-    var estado = document.getElementById("estado").value;
-    var cep = document.getElementById("cep").value;
-    var telefone = document.getElementById("telefone").value;
-    var cpfcnpj = document.getElementById("CpfCnpj").value;
-    var cliente = document.getElementById("cliente").value;
-    var telefone2 = document.getElementById("telefone2").value;
-    var cpfcnpj2 = document.getElementById("CpfCnpj2").value;
-    var obs = document.getElementById("obs").value;
-    var descricao = document.getElementById("descricao").value;
-    var servico = document.getElementById("servico").value;
-    var data = document.getElementById("data").value;
-    var hora = document.getElementById("hora").value;
-
-    if(nos.value != ""&&nome.value != ""&&endereco.value != ""&&numero.value != ""&&bairro.value != ""&&cidade.value != ""&&estado.value != ""&&cep.value != ""&&cpfcnpj.value != ""&&telefone.value != ""&&cliente.value != ""&&telefone2.value != ""&&cpfcnpj2.value != ""&&obs.value != ""&&descricao.value != ""&&servico.value != ""&&data.value != ""&&hora.value != ""){
-        var nos1=parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Nº OS: "+nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Nome: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Endereco: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Numero: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Bairro: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Cidade: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Estado: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "CEP: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Telefone: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "CPF ou CNPJ: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Cliente: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Telefone: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "CPF ou CNPJ: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Obs: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Descricao: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Servico: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Data: " + nos;
-        var nos1 = parseInt(nos.value);
-        document.getElementById("nos1").innerHTML = "Hora: " + nos;
-    }
-}
