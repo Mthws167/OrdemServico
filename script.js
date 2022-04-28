6331dea0d9f8cb4ac1add156a41e33c13e8746a4
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-28 11:51:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
+function esconderMostrar(){
+    var form2 = document.getElementById("form2").style.display;
+    if (form2=='none'){
+        document.getElementById("form2").style.display="block";
+    }else{
+        document.getElementById("form2").style.display = "none";
+    }
+}
+
 function resultado() {
     document.getElementById('form1').style.display = "none";
-    let nos = document.getElementById("nos").value;
-    let nome = document.getElementById("nome").value;
-    let endereco = document.getElementById("endereco").value;
-    let numero = document.getElementById("numero").value;
-    let bairro = document.getElementById("bairro").value;
-    let cidade = document.getElementById("cidade").value;
-    let estado = document.getElementById("estado").value;
-    let cep = document.getElementById("cep").value;
-    let telefone = document.getElementById("telefone").value;
-    let cpfcnpj = document.getElementById("CpfCnpj").value;
-    let cliente = document.getElementById("cliente").value;
-    let telefone2 = document.getElementById("telefone2").value;
-    let cpfcnpj2 = document.getElementById("CpfCnpj2").value;
-    let obs = document.getElementById("obs").value;
-    let descricao = document.getElementById("descricao").value;
-    let servico = document.getElementById("servico").value;
-    let data = document.getElementById("data").value;
-    let hora = document.getElementById("hora").value;
+    
+    var nos1 = document.getElementById("nos").value;
+    var nome1 = document.getElementById("nome").value;
+    var endereco1 = document.getElementById("endereco").value;
+    var numero1 = document.getElementById("numero").value;
+    var bairro1 = document.getElementById("bairro").value;
+    var cidade1 = document.getElementById("cidade").value;
+    var estado1 = document.getElementById("estado").value;
+    var cep1 = document.getElementById("cep").value;
+    var telefone1 = document.getElementById("telefone").value;
+    var cpfcnpj1 = document.getElementById("CpfCnpj").value;
+    var cliente1 = document.getElementById("cliente").value;
+    var telefone21 = document.getElementById("telefone2").value;
+    var cpfcnpj21 = document.getElementById("CpfCnpj2").value;
+    var obs1 = document.getElementById("obs").value;
+    var descricao1 = document.getElementById("descricao").value;
+    var servico1 = document.getElementById("servico").value;
+    var data1 = document.getElementById("data").value;
+    var hora1 = document.getElementById("hora").value;
 
-    if ( nos.value != ''  && nome.value != "" && endereco.value != "" && numero.value != "" && bairro.value != "" && cidade.value != "" && estado.value != "" && cep.value != "" && cpfcnpj.value != "" && telefone.value != "" && cliente.value != "" && telefone2.value != "" && cpfcnpj2.value != "" && obs.value != "" && descricao.value != "" && servico.value != "" && data.value != "" && hora.value != "") {
+    if (onclick.botao2 && nos1.value != ''  && nome1.value != "" && endereco1.value != "" && numero1.value != "" && bairro1.value != "" && cidade1.value != "" && estado1.value != "" && cep1.value != "" && cpfcnpj1.value != "" && telefone1.value != "" && cliente1.value != "" && telefone21.value != "" && cpfcnpj21.value != "" && obs1.value != "" && descricao1.value != "" && servico1.value != "" && data1.value != "" && hora1.value != "") {
         document.getElementById('form2').style.display = "block";
 
-        document.getElementById("nos1").innerHTML = "Nº OS: " + nos;
-        document.getElementById("nome1").innerHTML = "Nome: " + nome;
-        document.getElementById("endereco1").innerHTML = "Endereço: " + endereco;
-        document.getElementById("nmero1").innerHTML = "Número: " + numero;
-        document.getElementById("bairro1").innerHTML = "Bairro: " + bairro;
-        document.getElementById("cidade1").innerHTML = "Cidade: " + cidade;
-        document.getElementById("estado1").innerHTML = "Estado: " + estado;
-        document.getElementById("cep1").innerHTML = "CEP: " + cep;
-        document.getElementById("telefone1").innerHTML = "Telefone: " + telefone;
-        document.getElementById("cpfcnpj1").innerHTML = "CPF/CNPJ: " + cpfcnpj;
-        document.getElementById("cliente1").innerHTML = "Cliente: " + cliente;
-        document.getElementById("telefone21").innerHTML = "Telefone: " + telefone2;
-        document.getElementById("cpfcnpj21").innerHTML = "CPF/CNPJ: " + cpfcnpj2;
-        document.getElementById("obs1").innerHTML = "Obs.: " + obs;
-        document.getElementById("descricao1").innerHTML = "Descrição: " + descricao;
-        document.getElementById("servico1").innerHTML = "Serviço: " + servico;
-        document.getElementById("data1").innerHTML = data;
-        document.getElementById("hora1").innerHTML = hora;
+
+        document.getElementById("nos").innerHTML = "ORDEM DE SERVIÇO Nº: " + nos1;
+        document.getElementById("nome").innerHTML = nome1;
+        document.getElementById("enderecoNumeroBairro").innerHTML = endereco1 + "," + numero1 + "," + bairro1;
+        document.getElementById("cidadeEstadoCep").innerHTML = cidade1 + "" + estado1 + ", CEP:" + cep1;
+        document.getElementById("telefone").innerHTML = telefone1;
+        document.getElementById("cpfcnpj").innerHTML = "CPF/CNPJ: " + cpfcnpj1;
+        document.getElementById("cliente").innerHTML = "Cliente: " + cliente1;
+        document.getElementById("telefone2").innerHTML = "Telefone: " + telefone21;
+        document.getElementById("cpfcnpj2").innerHTML = "CPF/CNPJ: " + cpfcnpj21;
+        document.getElementById("obs").innerHTML = "OBS.: " + obs1;
+        document.getElementById("descricao").innerHTML = descricao1;
+        document.getElementById("servico").innerHTML = servico1;
+        document.getElementById("dataHora").innerHTML = data1 + '-' + hora1;
 
     }
 }
@@ -79,3 +85,7 @@ var CpfCnpjMask = function (val) {
     };
 
 
+function imprimir() {
+    window.print();
+}
+document.getElementById('botao').style.display ="none";
